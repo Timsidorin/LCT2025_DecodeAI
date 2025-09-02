fb28ee95b79d12906af01a080ad5e03da815d69d
TimofeyProjects <101704774+tim26006@users.noreply.github.com>
2025-09-02 11:19:01 +1000

    Начало микросервиса получения банковских продуктов

---
+++ ProductsScrapping_service/sources.py
@@ -0,0 +1,35 @@
+products = {
+    "cards": {
+        "Дебетовые карты": "https://www.gazprombank.ru/personal/cards/",
+        "Кредитные карты": "https://www.gazprombank.ru/personal/credit-cards/",
+        "Премиальные карты": "https://www.gazprombank.ru/premium/",
+        "Зарплатные карты": "https://www.gazprombank.ru/personal/page/isalary-cards/",
+        "Карты жителей": "https://www.gazprombank.ru/personal/city-cards/"
+    },
+
+    "deposits_and_accounts": {
+        "Накопительные счета": "https://www.gazprombank.ru/personal/accounts/",
+        "Вклады": "https://www.gazprombank.ru/personal/increase/deposits/"
+    },
+
+    "credits": {
+        "Кредиты под залог": "https://www.gazprombank.ru/personal/bail/",
+        "Потребительские кредиты": "https://www.gazprombank.ru/personal/take_credit/consumer_credit/"
+    },
+
+    "invest": {
+        "Инвестиционные продукты": "https://www.gazprombank.ru/personal/page/increase/investment/"
+    },
+
+    "autocredit": {
+        "Автокредит": "https://www.gazprombank.ru/personal/avtokredit/"
+    },
+
+    "mortgage": {
+        "Ипотека": "https://www.gazprombank.ru/personal/mortgage/"
+    },
+
+    "services": {
+
+    }
+}
