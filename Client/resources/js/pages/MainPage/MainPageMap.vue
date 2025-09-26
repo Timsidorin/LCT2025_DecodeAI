10e332cf8e7fee9146dcea695bbf18d96954381e
B2ng4 <kostyan2004tt@mail.ru>
2025-09-26 20:02:32 +1000

    Добавил графики, доработал страницу с картой

--- Client/resources/js/pages/MainPage/MainPageMap.vue
+++ Client/resources/js/pages/MainPage/MainPageMap.vue
@@ -3,9 +3,10 @@
         <div class="col-7">
             <main-page-map-russia />
         </div>
-        <div class="q-ml-auto q-mr-xl">
-            <main-page-map-date-picker/>
-            <main-page-map-table/>
+        <div class="q-ml-auto q-mr-xl column">
+            <main-page-map-date-picker class="q-mb-md"/>
+            <main-page-map-table class="q-mb-md"/>
+            <graph-source/>
         </div>
     </div>
 </template>
@@ -14,8 +15,5 @@
 import MainPageMapRussia from "../../components/pages/MainPage/Map/MainPageMapRussia.vue";
 import MainPageMapTable from "../../components/pages/MainPage/Map/MainPageMapTable.vue";
 import MainPageMapDatePicker from "../../components/pages/MainPage/Map/MainPageMapDatePicker.vue";
+import GraphSource from "../../components/graphs/GraphSource.vue";
 </script>
-
-<style scoped>
-
-</style>
